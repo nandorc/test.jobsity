12f3f0a45c9378f1883b8cf7b467abd0b020bf62
nandorc <60071444+nandorc@users.noreply.github.com>
2021-02-20 12:03:18 -0500

    Un cambio de prueba
    Elemento modificado para verificación de comandos

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home', ['name' => 'Daniel']);
 Route::prefix('user')->group(function () {
     Route::get('/{id?}', function ($id = 'No user specified') {
-        return 'User: ' . $id;
+        return 'UserId: ' . $id;
     })->whereAlpha('id');
 });
