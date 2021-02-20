52e7ede8ed7b1f59619933046c8dd05ffe39c0d6
nandorc <60071444+nandorc@users.noreply.github.com>
2021-02-19 20:49:01 -0500

    Simple Maintenance Blade Template

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,9 @@
+<html>
+
+<body>
+    <h1>Error 503</h1>
+    <p>Este es solo un mensaje para informar que algo no esta del todo bien, entonces seguramente la página este en
+        mantenimiento</p>
+</body>
+
+</html>
