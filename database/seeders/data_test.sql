bb738239936ecf51d7ea05e9e64d7b33181b71b5
nandorc <60071444+nandorc@users.noreply.github.com>
2021-02-23 06:57:12 -0500

    README

--- database/seeders/data_test.sql
+++
@@ -1,7 +0,0 @@
---
--- Test data for users
---
--- Defult password assigned as 1234
---
-insert into users (uid,pwd,currency) values
-    ('nando','$2y$10$KJgBUvY1lKKzpikYfSytqeBxmI8Ekv0wncXZpB6xXMW8NL.WhZcpW','COP');
